ad3c16ec6d3c48ccdb18a7ecf34359af5bbb6fce
Peter Turner <git-commit@flintsteel7.com>
2018-02-27 16:06:33 -0800

    add some fields to profile form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as R from 'ramda';
+import React from 'react';
+import { withHandlers, withState } from 'recompose';
+import { componentDidMount } from 'react-functional-lifecycle';
+import loadCurrentUser, { loadUserName } from './api';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const fetchUser = () =>
+  Promise.resolve(loadCurrentUser())
 
-export default App;
+const getUser = props => fetchUser().then(props.setUser)
+
+const App = props => (
+  <div className="App">
+    <header>
+      <h1>Welcome to Renew, {R.prop('first_name', props.user.profile)}</h1>
+    </header>
+    <p>
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+    <form>
+      <div className="form-group">
+        <label for="first-name">First Name</label>
+        <input id="first-name" value={R.prop('first_name', props.user.profile)} />
+      </div>
+      <div className="form-group">
+        <label for="first-name">Last Name</label>
+        <input id="last-name" value={R.prop('last_name', props.user.profile)} />
+      </div>
+      <div className="form-group">
+        <label for="email">Email address</label>
+        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" value={R.prop('email', props.user.profile)}/>
+        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div className="form-group">
+        <label for="time-zone">Time Zone</label>
+        <input className="form-control" id="time-zone" placeholder="Time Zone" value={R.prop('time_zone', props.user.profile)} />
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+  </div>
+);
+
+export default R.compose(
+  withState('user', 'setUser', {}),
+  withHandlers({
+    getUser
+  }),
+  componentDidMount(R.compose(
+    R.tap(getUser)
+  ))
+)(App);
---
+++ src/api.js
@@ -0,0 +1,25 @@
+export default function loadCurrentUser() {
+  return JSON.parse(`{
+    "projects": [],
+    "profile": {
+      "user_icon": "https://avatars0.githubusercontent.com/u/10226589?v=4",
+      "time_zone": "America/Los_Angeles",
+      "settings": null,
+      "screen_name": "flintsteel7",
+      "last_name": "Turner",
+      "first_name": "Peter",
+      "email": "git-commit@flintsteel7.com",
+      "auth": {
+        "github_username": "flintsteel7",
+        "github_access_token": "dRkz037ZXqTsVxZR/JTXoIJy0mWWOKyRzu98A1w+4GvVBlwZd+ZxsSq6Qx0EFqeE",
+        "auth0_uid": "github|10226589"
+      }
+    },
+    "id": 6374335981381742592,
+    "created_at": "2018-02-27T19:35:27.396Z"
+  }`)
+}
+
+export function loadUserName() {
+  return JSON.parse(`{ "first_name": "Peter" }`)
+}
