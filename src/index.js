72df29af936583f391028484626fc2654f162573
Kevin Old <kevin@kevinold.com>
2018-03-06 13:06:54 -0600

    Load user data when app is loaded by dispatching redux action

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/configureStore'
+import { myProfilePending } from './actions/AccountActions';
 
 const store = configureStore();
+
+// Load user data as app is loaded
+store.dispatch(myProfilePending());
 
 ReactDOM.render(
   <Provider store={store}>
